3ebb70708564a12fc26701555c83cc350d95c965
techsayright <darshan2001.dp@gmail.com>
2022-11-10 18:14:17 +0530

    msk init

--- src_postgres/postgres.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE class;
-\connect class;
-
-CREATE TABLE subjects (subject_id INTEGER PRIMARY KEY, subject_name TEXT, course_id INTEGER );
-
-\copy subjects FROM '/home/config/Data/subject.csv' DELIMITER ',' CSV HEADER;
-
